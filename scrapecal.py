6be97546189ca6afbd42889413633fa995044c1a
amicha02 <andreaszmichael6@gmail.com>
2020-09-16 02:07:07 -0400

    ALMOST DONE. INTEGRATED GOOGLE CALENDAR API WITH THE PROJECT

--- scrapecal.py
+++ scrapecal.py
@@ -13,6 +13,8 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 import locale
+from dateutil import parser
+import pickle
 locale.setlocale(locale.LC_ALL, 'de_DE')
 req.url
 req = requests.get("https://www.ccny.cuny.edu/registrar/fall-2018-academic-calendar")
@@ -23,8 +25,6 @@ soup = BS(req.text, 'html.parser')
 print(soup.prettify)
 soup.title
 soup.p
-
-
 data_rows = [row for row in soup.find_all('tr')]
 value_series = [row.find_all('td') for row in data_rows]
 value_series = [[cell.get_text().strip() for cell in row.find_all('td')] for row in data_rows]
@@ -37,12 +37,41 @@ print(df)
 
 
 
-df.to_csv(r'C:\Users\User\Desktop\try1.csv', index = False)
-str_dates = list(df['dates'])
+# Integrating Google Calendar API in Python Projects
+from apiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+scopes = ['https://www.googleapis.com/auth/calendar']
+flow = InstalledAppFlow.from_client_secrets_file('C:\\Users\\User\Desktop\\client_secret.json',scopes=scopes)
+#credentials = flow.run_console()
+credentials
+pickle.dump(credentials,open('token.pkl','wb'))
+credentials = pickle.load(open('token.pkl','rb'))
+credentials
+service = build('calendar','v3',credentials=credentials)
+result = service.calendarList().list().execute()
+calendar_id = result['items'][0]['id']
+
+string = 'December 28'
+type(parser.parse(string).strftime('%m-%d'))
+string = 'December 28'
+type(parser.parse(string).strftime('%m-%d'))
 
 
 
-str_dates[0].isoformat()
+#EVENT FORMAT
+event = {
+  'summary': 'Google I/O 2015',
+  'description': 'Event',
+  'start': {
+    'dateTime': '2020-09-19T09:00:00-07:00',
+    'timeZone': 'America/New_York',
+  },
+  'end': {
+    'dateTime': '2020-09-19T09:00:00-07:00',
+    'timeZone': 'America/New_York',
+  },
+}
+created_calendar = service.events().insert(calendarId=calendar_id,body=event).execute()
 
 
 
@@ -54,18 +83,6 @@ str_dates[0].isoformat()
 
 
 
-all_p = soup.find_all('p')
-len(all_p)
-all_tables = soup.find_all('table')
-len(all_tables)   
-col1 = []
-col2 =[]
-col3 = []
-
-for rowtag in all_p:
-    #row = str(rowtag)
- #   row = row.strip('<p>').strip('</p>').strip('strong>').strip('</')
-    print(rowtag.get_text())
 
 
 
