b6c50790792b1d4f717dfba8738b6cc502432d3c
amicha02 <andreaszmichael6@gmail.com>
2023-11-24 15:10:17 -0500

    "Update"

--- scrapecal.py
+++ scrapecal.py
@@ -1,12 +1,4 @@
 
-#Your mission is to scrape the cuny fall 2018 academic calendar site using the requests library,
-#beautiful soup and pandas, just like in class.
-#You should wind up with a pandas data frame where the index column is a python date. 
-#There should be a column for "day of the week" with variable lable dow and a column called text with the explanation. 
-# cols = date,day_of_the_week,explanation
-#Print out this data frame. 
-#5 extra credit points if you figure out how to use the google api to programatically add these dates to your google calendar with the request library
-
 import requests
 from bs4 import BeautifulSoup as BS
 import pandas as pd
@@ -22,7 +14,7 @@ req.status_code
 req.text
 
 soup = BS(req.text, 'html.parser')
-print(soup.prettify)
+
 soup.title
 soup.p
 data_rows = [row for row in soup.find_all('tr')]
